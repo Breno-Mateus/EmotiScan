7249c573193a7b375edc890a924ba3d0eb432302
Breno-Mateus <brenomateus36@gmail.com>
2025-06-14 18:43:31 -0300

    feat: add browser api

--- src/components/analysisSection.tsx
+++ src/components/analysisSection.tsx
@@ -1,11 +1,33 @@
+import { useEffect, useRef } from "react";
+
 const AnalysisSection = () => {
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    async function startCamera() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (err) {
+        console.error("Erro ao acessar a câmera:", err);
+      }
+    }
+
+    startCamera();
+  }, []);
+
   return (
     <section className="flex flex-col gap-4 text-black w-1/2 justify-center items-center">
-      <div className="bg-white rounded-md w-3/4 h-3/5 p-2">
-        <div className="bg-gray-300 rounded-md h-full">oi</div>
+      <div className="bg-white rounded-md p-2">
+        <div className="bg-gray-300 rounded-md flex justify-center">
+          <video ref={videoRef} autoPlay playsInline className="rounded-md"/>
+        </div>
       </div>
 
-      <div className="bg-white rounded-md w-3/4 h-1/6">olá</div>
+      <div className="bg-white rounded-md p-2 w-full">olá</div>
     </section>
   );
 };
