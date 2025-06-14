86e72bfdf147e91834f370edc9b169a7807b23b9
Breno-Mateus <brenomateus36@gmail.com>
2025-06-14 18:20:47 -0300

    add componentes

---
+++ src/components/analysisSection.tsx
@@ -0,0 +1,13 @@
+const AnalysisSection = () => {
+  return (
+    <section className="flex flex-col gap-4 text-black w-1/2 justify-center items-center">
+      <div className="bg-white rounded-md w-3/4 h-3/5 p-2">
+        <div className="bg-gray-300 rounded-md h-full">oi</div>
+      </div>
+
+      <div className="bg-white rounded-md w-3/4 h-1/6">olá</div>
+    </section>
+  );
+};
+
+export default AnalysisSection;
